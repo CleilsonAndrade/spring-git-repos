85c8f4e6b179ae4a0aac08dbcbf144dff12c24c3
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-02-22 16:58:55 -0300

    ✨ feat: bean configuration github client config

---
+++ src/main/java/br/com/cleilsonandrade/springgitrepos/api/GithubClientConfig.java
@@ -0,0 +1,24 @@
+package br.com.cleilsonandrade.springgitrepos.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class GithubClientConfig {
+  @Bean
+  public HttpServiceProxyFactory httpServiceProxyFactory() {
+    WebClient webClient = WebClient.builder()
+        .baseUrl("https://api.github.com")
+        .build();
+
+    return HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
+  }
+
+  @Bean
+  public GithubClient githubClient(HttpServiceProxyFactory factory) {
+    return factory.createClient(GithubClient.class);
+  }
+}
