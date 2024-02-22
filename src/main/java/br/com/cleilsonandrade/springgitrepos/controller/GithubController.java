a420969b3fc062e8d54c4ecfa0e726ef983c7b82
CleilsonAndrade <cleilsonjose@hotmail.com>
2024-02-22 16:51:28 -0300

    ✨ feat: inject github client in controller

--- src/main/java/br/com/cleilsonandrade/springgitrepos/controller/GithubController.java
+++ src/main/java/br/com/cleilsonandrade/springgitrepos/controller/GithubController.java
@@ -2,16 +2,25 @@ package br.com.cleilsonandrade.springgitrepos.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cleilsonandrade.springgitrepos.api.GithubClient;
 
 @RestController
 @RequestMapping("/repos")
 public class GithubController {
+  @Autowired
+  private GithubClient githubClient;
+
   @GetMapping
-  public ResponseEntity<List<RepositoryResponse>> listRepos() {
-    return null;
+  public ResponseEntity<List<RepositoryResponse>> listRepos(@RequestHeader("token") String token) {
+    var repos = githubClient.listRepos("Bearer " + token, null, "public");
+
+    return ResponseEntity.ok(repos);
   }
 }
